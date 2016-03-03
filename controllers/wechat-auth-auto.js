c4b3da00378544b81020b0a5565a35cee34fd793
汤丰源 <1315803594@qq.com>
2016-03-03 20:50:35 +0800

    改变端口号

--- controllers/wechat-auth-auto.js
+++ controllers/wechat-auth-auto.js
@@ -4,11 +4,10 @@
 var wechat = require('co-wechat');
 module.exports = function(app){
     app.use(wechat('wechat').middleware(function* (){
-        console.log('----------------------');
         var message = this.weixin;
         console.log('message-->>',message);
         if(message.MsgType === 'text'){
-            this.body = "我爱你 小俊俊";
+            this.body = "我爱你 老婆";
         }
     }))
 }
