bf7eb0183fe233e1551703e0d12176379fd4660b
汤丰源 <1315803594@qq.com>
2016-03-04 12:06:51 +0800

    no message

--- controllers/wechat/wechat-auth-auto.js
+++ controllers/wechat/wechat-auth-auto.js
@@ -41,7 +41,7 @@ module.exports = function(app){
         if(message.Event === 'subscribe'){
             const openid = message.FromUserName;
             console.log('openid--->>',openid);
-            const userInfo = api.getUser(openid);
+            const userInfo = yield api.getUser(openid);
             console.log('userInfo-->>',userInfo);
             this.body = userInfo;
         }
